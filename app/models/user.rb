1635371ca8ef5da8a2dd83d15e40fa092f53a712
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-07-12 07:41:16 +0000

    [Finish]4-9

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+         has_many:post_images,dependent: :destroy #userが削除された時アソシエーションした情報も削除される
+end
